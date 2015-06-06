1699af0fb5f79e0006a446b04e8d8385df37a55d
Erko Bridee <erko.bridee@gmail.com>
2015-06-06 00:54:35 -0300

    gulp-steps : 01 - build task defined

--- 01/gulpfile.js
+++ 01/gulpfile.js
@@ -29,15 +29,16 @@ function projectInfoMsg() {
 //==============================================================================
 // @begin: gulp tasks ==========================================================
 //==============================================================================
-
 // clean
 gulp.task('clean', del.bind(null, [ 'dist' ]));
 
+//------------------------------------------------------------------------------
 // jshint
 gulp.task('jshint:tools', function() { console.log('TODO: define'); });
 gulp.task('jshint:project', function() { console.log('TODO: define'); });
 gulp.task('jshint', ['jshint:tools', 'jshint:project']);
 
+//------------------------------------------------------------------------------
 // lintspaces
 gulp.task('lintspaces:tools', function() { console.log('TODO: define'); });
 gulp.task('lintspaces:project:js', function() { console.log('TODO: define'); });
@@ -46,17 +47,49 @@ gulp.task('lintspaces:project:html', function() { console.log('TODO: define'); }
 gulp.task('lintspaces:project', ['lintspaces:project:js', 'lintspaces:project:css', 'lintspaces:project:html']);
 gulp.task('lintspaces', ['lintspaces:tools', 'lintspaces:project']);
 
+//------------------------------------------------------------------------------
 // validate
 gulp.task('validate', ['jshint', 'lintspaces']);
 
-// build
-gulp.task('build', ['validate'], function() { console.log('TODO: define'); });
+//------------------------------------------------------------------------------
+// @begin: build
+gulp.task('build', ['clean', 'validate'], function() {
 
+  var jsFilter       = filter( '**/*.js' ),
+      cssFilter      = filter( '**/*.css' ),
+      htmlFilter     = filter( '**/*.html' ),
+      userefAssets   = useref.assets(),
+      minifyHtmlOpts = {
+        conditionals: true,
+        spare:true
+      };
+
+  return gulp.src( 'src/index.html' )
+    .pipe( userefAssets ) // Concatenate with gulp-useref
+    .pipe( jsFilter )
+    .pipe( uglify() ) // Minify any javascript sources
+    .pipe( jsFilter.restore() )
+    .pipe( cssFilter )
+    .pipe( csso() ) // Minify any CSS sources
+    .pipe( cssFilter.restore() )
+    .pipe( rev() ) // Rename the concatenated files
+    .pipe( userefAssets.restore() )
+    .pipe( useref() )
+    .pipe( revReplace() ) // Substitute in new filenames
+    .pipe( htmlFilter )
+    .pipe( minifyHtml( minifyHtmlOpts ) )
+    .pipe( htmlFilter.restore() )
+    .pipe( gulp.dest('dist') );
+
+});
+// @end: build
+//------------------------------------------------------------------------------
 // webserver
 gulp.task('webserver:dist', ['build'], function() { console.log('TODO: define'); });
 
 gulp.task('webserver:dev', ['validate'], function() { console.log('TODO: define'); });
 
+//------------------------------------------------------------------------------
 // watch
 gulp.task('watch', ['webserver:dev'], function() { console.log('TODO: define'); });
 
