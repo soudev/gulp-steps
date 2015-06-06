b39ec9360d0c23b613475c348642080ea843c93e
Erko Bridee <erko.bridee@gmail.com>
2015-06-06 20:14:41 -0300

    gulp step 02 - jshint tasks updated

--- 02/gulpfile.js
+++ 02/gulpfile.js
@@ -118,18 +118,21 @@ gulp.task('bower:dist', ['bower:dev'], function() {
 //------------------------------------------------------------------------------
 // @begin: jshint
 
+$.streams.jshint = $.lazypipe()
+  .pipe( $.cached, 'jshint' )
+  .pipe( $.jshint )
+  .pipe( $.jshint.reporter, 'jshint-stylish' )
+  .pipe( $.jshint.reporter, 'fail' );
+
+
 gulp.task('jshint:tools', function() {
-  return gulp.src('gulpfile.js')
-    .pipe( jshint() )
-    .pipe( jshint.reporter('jshint-stylish') )
-    .pipe( jshint.reporter('fail') );
+  return gulp.src( $.config.tools )
+    .pipe( $.streams.jshint() );
 });
 
 gulp.task('jshint:project', function() {
-    return gulp.src('src/**/*.js')
-    .pipe( jshint() )
-    .pipe( jshint.reporter('jshint-stylish') )
-    .pipe( jshint.reporter('fail') );
+  return gulp.src( $.config.project.js )
+    .pipe( $.streams.jshint() );
 });
 
 gulp.task('jshint', ['jshint:tools', 'jshint:project']);
