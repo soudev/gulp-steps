e4fb1a2d934969445af40eac590f786c80bbd5ba
Erko Bridee <erko.bridee@gmail.com>
2015-06-08 11:51:22 -0300

    gulp step 03 - gulp jshint task moved from gulpfile.js to gulp/tasks/jshint.js

---
+++ 03/gulp/tasks/jshint.js
@@ -0,0 +1,24 @@
+
+var gulp        = require('gulp'),
+    $           = require('../$');
+
+//---
+
+$.streams.jshint = $.lazypipe()
+  .pipe( $.cached, 'jshint' )
+  .pipe( $.jshint )
+  .pipe( $.jshint.reporter, 'jshint-stylish' )
+  .pipe( $.jshint.reporter, 'fail' );
+
+
+gulp.task('jshint:tools', function() {
+  return gulp.src( $.config.tools )
+    .pipe( $.streams.jshint() );
+});
+
+gulp.task('jshint:project', function() {
+  return gulp.src( $.config.project.js )
+    .pipe( $.streams.jshint() );
+});
+
+gulp.task('jshint', ['jshint:tools', 'jshint:project']);
--- 03/gulpfile.js
+++ 03/gulpfile.js
@@ -9,29 +9,6 @@ requireDir( './gulp/tasks');
 //==============================================================================
 // @begin: gulp tasks ==========================================================
 //==============================================================================
-// @begin: jshint
-
-$.streams.jshint = $.lazypipe()
-  .pipe( $.cached, 'jshint' )
-  .pipe( $.jshint )
-  .pipe( $.jshint.reporter, 'jshint-stylish' )
-  .pipe( $.jshint.reporter, 'fail' );
-
-
-gulp.task('jshint:tools', function() {
-  return gulp.src( $.config.tools )
-    .pipe( $.streams.jshint() );
-});
-
-gulp.task('jshint:project', function() {
-  return gulp.src( $.config.project.js )
-    .pipe( $.streams.jshint() );
-});
-
-gulp.task('jshint', ['jshint:tools', 'jshint:project']);
-
-// @end: jshint
-//------------------------------------------------------------------------------
 // @begin: lintspaces
 
 $.streams.lintspaces = $.lazypipe()
