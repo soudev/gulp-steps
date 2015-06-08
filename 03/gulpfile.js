da59b7ae2a2c0a9657228761ee3ef5782c7f4158
Erko Bridee <erko.bridee@gmail.com>
2015-06-08 11:57:57 -0300

    gulp step 03 - gulp build tasks moved from gulpfile.js to gulp/tasks/build.js

--- 03/gulpfile.js
+++ 03/gulpfile.js
@@ -9,42 +9,6 @@ requireDir( './gulp/tasks');
 //==============================================================================
 // @begin: gulp tasks ==========================================================
 //==============================================================================
-// @begin: build
-
-gulp.task('build:index', function() {
-
-  var jsFilter       = $.filter( '**/*.js' ),
-      stylesFilter   = $.filter( '**/*.css' ),
-      htmlFilter     = $.filter( '**/*.html' ),
-      userefAssets   = $.useref.assets(),
-      minifyHtmlOpts = {
-        conditionals: true,
-        spare:true
-      };
-
-  return gulp.src( $.config.project.index )
-    .pipe( userefAssets ) // Concatenate with gulp-useref
-    .pipe( jsFilter )
-    .pipe( $.uglify() ) // Minify any javascript sources
-    .pipe( jsFilter.restore() )
-    .pipe( stylesFilter )
-    .pipe( $.csso() ) // Minify any CSS sources
-    .pipe( stylesFilter.restore() )
-    .pipe( $.rev() ) // Rename the concatenated files
-    .pipe( userefAssets.restore() )
-    .pipe( $.useref() )
-    .pipe( $.revReplace() ) // Substitute in new filenames
-    .pipe( htmlFilter )
-    .pipe( $.minifyHtml( minifyHtmlOpts ) )
-    .pipe( htmlFilter.restore() )
-    .pipe( gulp.dest( $.config.paths.dist ) );
-
-});
-
-gulp.task('build', $.sequence(['clean:dist', 'validate'], ['build:index', 'bower:dist']));
-
-// @end: build
-//------------------------------------------------------------------------------
 // @begin: webserver
 
 gulp.task('webserver:dist', ['build'], function() {
