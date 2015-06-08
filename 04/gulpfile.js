43118daa583556c04a59995a92504333c914383a
Erko Bridee <erko.bridee@gmail.com>
2015-06-08 15:24:20 -0300

    gulp step 04 - tools/gulp/helpers/loadTasks.js defined

--- 04/gulpfile.js
+++ 04/gulpfile.js
@@ -1,9 +1,14 @@
 
+/*
+  - load gulp tasks files
+    - main tasks and default gulp task defined on gulp/tasks/default.js
+*/
+/*
+require('./tools/gulp')
+  .loadTasks('tools/gulp/tasks');
+*/
+
 var requireDir  = require( 'require-dir' );
 
 // Require all gulp tasks
 requireDir( './tools/gulp/tasks');
-
-/*
-  main tasks and default gulp task moved to gulp/tasks/default.js
-*/
---
+++ 04/tools/gulp/index.js
@@ -0,0 +1,2 @@
+
+module.exports.loadTasks = require('./helpers/loadTasks');
