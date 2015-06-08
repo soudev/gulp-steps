ea113e5d8468af6c68e91b20ab4746977d058e24
Erko Bridee <erko.bridee@gmail.com>
2015-06-08 11:53:53 -0300

    gulp step 03 - gulp lintspaces tasks moved from gulpfile.js to gulp/tasks/lintspaces.js

---
+++ 03/gulp/tasks/lintspaces.js
@@ -0,0 +1,39 @@
+
+var gulp        = require('gulp'),
+    $           = require('../$');
+
+//---
+
+$.streams.lintspaces = $.lazypipe()
+  .pipe( $.cached, 'lintspaces' )
+  .pipe( $.lintspaces, { editorconfig: $.config.paths.editorconfig } )
+  .pipe( $.lintspaces.reporter );
+
+
+gulp.task('lintspaces:tools', function() {
+  return gulp.src( $.config.tools )
+    .pipe( $.streams.lintspaces() );
+});
+
+gulp.task('lintspaces:project:js', function() {
+  return gulp.src( $.config.project.js )
+    .pipe( $.streams.lintspaces() );
+});
+
+gulp.task('lintspaces:project:styles', function() {
+  return gulp.src( $.config.project.styles )
+    .pipe( $.streams.lintspaces() );
+});
+
+gulp.task('lintspaces:project:html', function() {
+  return gulp.src( $.config.project.html )
+    .pipe( $.streams.lintspaces() );
+});
+
+gulp.task('lintspaces:project', [
+  'lintspaces:project:js',
+  'lintspaces:project:styles',
+  'lintspaces:project:html'
+]);
+
+gulp.task('lintspaces', ['lintspaces:tools', 'lintspaces:project']);
--- 03/gulpfile.js
+++ 03/gulpfile.js
@@ -9,44 +9,6 @@ requireDir( './gulp/tasks');
 //==============================================================================
 // @begin: gulp tasks ==========================================================
 //==============================================================================
-// @begin: lintspaces
-
-$.streams.lintspaces = $.lazypipe()
-  .pipe( $.cached, 'lintspaces' )
-  .pipe( $.lintspaces, { editorconfig: $.config.paths.editorconfig } )
-  .pipe( $.lintspaces.reporter );
-
-
-gulp.task('lintspaces:tools', function() {
-  return gulp.src( $.config.tools )
-    .pipe( $.streams.lintspaces() );
-});
-
-gulp.task('lintspaces:project:js', function() {
-  return gulp.src( $.config.project.js )
-    .pipe( $.streams.lintspaces() );
-});
-
-gulp.task('lintspaces:project:styles', function() {
-  return gulp.src( $.config.project.styles )
-    .pipe( $.streams.lintspaces() );
-});
-
-gulp.task('lintspaces:project:html', function() {
-  return gulp.src( $.config.project.html )
-    .pipe( $.streams.lintspaces() );
-});
-
-gulp.task('lintspaces:project', [
-  'lintspaces:project:js',
-  'lintspaces:project:styles',
-  'lintspaces:project:html'
-]);
-
-gulp.task('lintspaces', ['lintspaces:tools', 'lintspaces:project']);
-
-// @end: lintspaces
-//------------------------------------------------------------------------------
 
 gulp.task('validate', ['jshint', 'lintspaces']);
 
